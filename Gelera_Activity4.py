88a7c2b2cb93995614cf6df4a6144f95cd77a1e6
AlvinGelera <145734407+AlvinGelera@users.noreply.github.com>
2023-09-22 12:03:21 +0800

    Add files via upload

---
+++ Gelera_Activity4.py
@@ -0,0 +1,45 @@
+print("Hello Peeps!")
+print("This is from a line of code")
+print("5")
+print(5.5)
+print("Hello, World")
+
+#1
+x=50
+y=29
+z=x+y
+print(z,"(sum)")
+
+#2
+a=7
+product=5*a
+print(product)
+
+#3
+e=2
+b=11
+e=e*2
+c=e*b
+b=b+(c*3)
+print(c)
+
+#4
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+sum = num1 + num2
+print("The sum of the two numbers is:", sum)
+#5
+squared = 3 ** 2
+cubed = 3 ** 3
+print("Squared:", squared)
+print("Cubed:", cubed)
+
+#6
+Name = "Mine"
+print("Your name is", Name)
+
+#7
+Apple_price = 20
+Orange_price = 20
+result = Apple_price == Orange_price
+print(result)
